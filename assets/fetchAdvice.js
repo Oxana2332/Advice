5a9d388dd34e8332e06a702bef3f61329d228b80
Oxana2332 <far_north@ro.ru>
2021-11-23 13:08:05 +0300

    del idAdvice, +adaptiv767px

--- assets/fetchAdvice.js
+++ assets/fetchAdvice.js
@@ -4,7 +4,6 @@ fetch("https://api.adviceslip.com/advice")
 .then(response => response.json())
 .then(slip => 
     {
-        document.getElementById('id').innerText=slip.slip.id;
         document.getElementById('advice').innerText=slip.slip.advice;
     })
 .catch(error => console.log(error));
